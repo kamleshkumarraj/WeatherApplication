bab99fa6bb6d819a64a129254310a14713b5f3ca
Kamlesh Kumar <kushwahakamleshraj434@gmail.com>
2024-04-26 11:49:10 +0530

    Finnaly we done full setup without any error tailwind with react and using parcel Bundler

--- Component/App.js
+++
@@ -1,8 +0,0 @@
-import Card from "./card";
-export default function App() {
-  return (
-    <div>
-      <Card/>
-    </div>
-  )
-}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { createRoot }  from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import './input.css';
 import App from "../Component/App";
+// import ReactDOM from "react-dom/client";
 
 const section = document.querySelector(".main-section");
-
 
 const root = createRoot(section);
 
